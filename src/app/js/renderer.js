63a193a5aba2af6c4fb7a2fd3a41d472ad769bc4
Mathis Laliberte <mathislaliberte@gmai.com>
2023-03-09 21:47:03 -0500

    Finished buble (password container as i called them)

--- src/app/js/renderer.js
+++ src/app/js/renderer.js
@@ -1,9 +1,8 @@
-const information = document.getElementById('info')
-information.innerText = `${versions.node()}`
+const body = document.getElementsByTagName('body')[0];
+const password = document.getElementsByClassName('appPassword')[0];
 
-const func = async () => {
-    const response = await window.versions.ping()
-    console.log(response) // prints out 'pong'
-  }
-  
-  func()
+//fill the password field with star characters
+for (let i = 0; i < 18; i++) {
+    password.innerHTML += '*';
+}
+// Path: src\app\js\main.js
